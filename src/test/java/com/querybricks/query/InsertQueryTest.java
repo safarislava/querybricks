f9bbfcd923542d6b68f3b908f3eb2a79850b89a1
safarislava <safarislava@gmail.com>
2026-06-30 19:22:41 +0300

    test: Increase branch coverage

--- src/test/java/com/querybricks/query/InsertQueryTest.java
+++ src/test/java/com/querybricks/query/InsertQueryTest.java
@@ -8,28 +8,70 @@ import com.querybricks.table.FakeTable;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 final class InsertQueryTest {
-    private final Query query = new InsertQuery(
-        new FakeTable("users"),
-        List.of(
-            new TableColumn<>(new FakeTable("users"), new RawColumn<>("id")).unbound(),
-            new TableColumn<>(new FakeTable("users"), new RawColumn<>("username")).unbound()
-        ),
-        List.of(
-            new InsertRow(
-                new NumberLiteral(1),
-                new StringLiteral("john")
-            )
-        )
-    );
-
     @Test
     void testSql() {
+        Query query = new InsertQuery(
+            new FakeTable("users"),
+            List.of(
+                new TableColumn<>(new FakeTable("users"), new RawColumn<>("id")).unbound(),
+                new TableColumn<>(new FakeTable("users"), new RawColumn<>("username")).unbound()
+            ),
+            List.of(
+                new InsertRow(
+                    new NumberLiteral(1),
+                    new StringLiteral("john")
+                )
+            )
+        );
         MatcherAssert.assertThat(
-            this.query.sql(),
+            query.sql(),
             Matchers.equalTo("INSERT INTO users (id, username) VALUES (1, 'john')")
         );
+    }
+
+    @Test
+    void testSqlWithEmptyColumns() {
+        Query query = new InsertQuery(
+            new FakeTable("users"),
+            List.of(),
+            List.of(
+                new InsertRow(
+                    new NumberLiteral(1),
+                    new StringLiteral("john")
+                )
+            )
+        );
+        try {
+            query.sql();
+        } catch (IllegalStateException e) {
+            MatcherAssert.assertThat(
+                e.getMessage(),
+                Matchers.equalTo("Insert query must specify at least one column")
+            );
+        }
+    }
+
+    @Test
+    void testSqlWithEmptyRows() {
+        Query query = new InsertQuery(
+            new FakeTable("users"),
+            List.of(
+                new TableColumn<>(new FakeTable("users"), new RawColumn<>("id")).unbound(),
+                new TableColumn<>(new FakeTable("users"), new RawColumn<>("username")).unbound()
+            ),
+            List.of()
+        );
+        try {
+            query.sql();
+        } catch (IllegalStateException e) {
+            MatcherAssert.assertThat(
+                e.getMessage(),
+                Matchers.equalTo("Insert query must have at least one row")
+            );
+        }
     }
 }
